a1875945e9b49b2aefcb660e019f540d0e5c218f
DS-jr <dmitry.k.sokolov@gmail.com>
2024-03-02 11:38:06 +0400

    dependencies, configs, Vitest

---
+++ tests/sample1.spec.ts
@@ -0,0 +1,7 @@
+import {describe, expect, it} from "vitest";
+
+describe("saMplE tesT bLoCk", () => {
+    it("sample test mUsT be tRue", () => {
+        expect(15-3).toBe(7+5);
+    })
+})
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		svelte({ hot: !process.env.VITEST }),
+	],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+	  },
 });
