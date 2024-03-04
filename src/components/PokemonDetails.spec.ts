09cbcc2f099c07c48104c39ea764962cfa212594
DS-jr <dmitry.k.sokolov@gmail.com>
2024-03-04 18:53:41 +0400

    first tests for the component

---
+++ src/components/PokemonDetails.spec.ts
@@ -0,0 +1,28 @@
+import {describe, beforeEach, it, expect} from "vitest";
+import { render } from '@testing-library/svelte';
+import PokemonDetails from './PokemonDetails.svelte'; // ?
+
+describe("PoKe detAils", () => {
+    
+    it("must show a loading spinner when making the API Call", () => {
+        const {getByText} = render(PokemonDetails);
+
+        expect(() => getByText(/Loading.../i)).not.toThrow(); 
+    })
+})
+
+
+
+// describe("Pokemon Details", () => {
+
+//     let instance = null;
+//     beforeEach(() => {
+//         const host = document.createElement('div');
+//         document.body.append(host);
+//         instance = new PokemonDetails({ target: host});
+//     })
+
+//     it('Should show a loading spinner when making the API Call', () => {
+//         expect(instance).toBeTruthy();
+//     })
+// })
---
+++ src/components/PokemonDetails.svelte
@@ -0,0 +1,2 @@
+<h2>There are NO shortcuts to any place worth going</h2>
+<h3>(Martin L. King) “You don't have to see the whole staircase. Just take the first step.”</h3>
