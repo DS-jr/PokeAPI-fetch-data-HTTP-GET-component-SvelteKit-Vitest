fd9abc7a88378fd5943861f3aa111eefd9b6f476
DS-jr <dmitry.k.sokolov@gmail.com>
2024-03-09 21:10:53 +0400

    added async test in Vitest

--- src/components/PokemonDetails.spec.ts
+++ src/components/PokemonDetails.spec.ts
@@ -1,15 +1,42 @@
-import {describe, beforeEach, it, expect} from "vitest";
-import { render } from '@testing-library/svelte';
+import {describe, beforeEach, it, expect, vi} from "vitest";
+import { render, waitFor } from '@testing-library/svelte';
 import PokemonDetails from './PokemonDetails.svelte'; // ?
 
 describe("PoKe detAils", () => {
     
-    it("must show a loading spinner when making the API Call", () => {
+    beforeEach(() => {
+        global.fetch = vi.fn().mockImplementation(() => {
+            return Promise.resolve({
+                json() {
+                  return Promise.resolve({name: 'TeSt poKe', height: 3, weight: 20, sprites: {front_default: ''}});
+                }
+            });
+        });
+    });
+
+    it('Should show a loading message when making the API Call', () => {
         const {getByText} = render(PokemonDetails);
 
-        expect(() => getByText(/Loading.../i)).not.toThrow(); 
+        expect(() => getByText(/Loading.../i)).not.toThrow();
     })
+
+    it('should show the data',async () => {
+        const {getByText} = render(PokemonDetails);
+
+        await waitFor(() => getByText(/Pokemon: Test Poke/i));
+        await waitFor(() => getByText(/Height: 3/i));
+        await waitFor(() => getByText(/Weight: 20/i));
+    })
+
 })
+    
+
+//     it("must show a loading spinner when making the API Call", () => {
+//         const {getByText} = render(PokemonDetails);
+
+//         expect(() => getByText(/Loading.../i)).not.toThrow(); 
+//     })
+// })
 
 
 
